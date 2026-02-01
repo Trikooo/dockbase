24a03c8e66417572fff1b0caf2ed3dbe43ec96ca
Yacine Zendaoui <trikooplays@gmail.com>
2026-02-01 15:14:33 +0100

    feat(disk): implement disk scheduler background worker

--- src/storage/disk/disk_scheduler.rs
+++ src/storage/disk/disk_scheduler.rs
@@ -1,8 +1,9 @@
+use std::sync::Arc;
 use std::sync::mpsc::Sender;
-use std::thread::JoinHandle;
+use std::thread::{JoinHandle};
 
 use crate::common::channel::Channel;
-use crate::common::config::PageId;
+use crate::common::config::{DOCKBASE_PAGE_SIZE, PageId};
 use crate::storage::disk::disk_manager::DiskManager;
 
 pub enum RequestType {
@@ -16,7 +17,26 @@ pub struct DiskRequest {
     pub callback: Sender<bool>,
 }
 pub struct DiskScheduler {
-    disk_manager: DiskManager,
-    request_queue: Channel<Option<DiskRequest>>,
+    disk_manager: Arc<DiskManager>,
+    request_queue: Arc<Channel<Option<DiskRequest>>>,
     background_thread: Option<JoinHandle<()>>,
 }
+
+impl DiskScheduler {
+
+    pub fn start_worker_thread(
+        disk_manager: Arc<DiskManager>,
+        queue: Arc<Channel<Option<DiskRequest>>>,
+    ) {
+        while let Ok(Some(request)) = queue.get() {
+            let page_data =
+                unsafe { std::slice::from_raw_parts_mut(request.data, DOCKBASE_PAGE_SIZE) };
+            let result = match request.request_type {
+                RequestType::Read => disk_manager.read_page(request.page_id, page_data),
+                RequestType::Write => disk_manager.write_page(request.page_id, page_data),
+            };
+            let _ = request.callback.send(result.is_ok());
+        }
+    }
+}
+
